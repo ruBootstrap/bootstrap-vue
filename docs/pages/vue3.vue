c48b2b10e84ef39bf163e99e6c3ae701e768cd71
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-05-03 22:42:27 +0300

    Перевел vue3

--- docs/pages/vue3.vue
+++ docs/pages/vue3.vue
@@ -1,39 +1,39 @@
 <template>
   <b-container id="content" fluid="lg" tag="main" class="py-5">
     <header class="pb-4 bd-content">
-      <h1>Vue.js 3.x initial support</h1>
+      <h1>Начальная поддержка Vue.js 3.x</h1>
       <p class="bd-lead">
-        BootstrapVue meets <code class="text-nowrap" translate="no">@vue-compat</code>!
+        BootstrapVue соответствует <code class="text-nowrap" translate="no">@vue-compat</code>!
       </p>
     </header>
     <section class="bd-content">
-      With the release of <b>v2.23.0</b> you can now use  <span class="bd-text-purple-bright">BootstrapVue</span> with <a href="https://v3-migration.vuejs.org/migration-build.html" target="_blank">migration build</a> of Vue.js 3
+      С выпуском <b>v2.23.0</b> теперь вы можете использовать <span class="bd-text-purple-bright">BootstrapVue</span> с <a href="https://v3-migration.vuejs.org/migration-build.html" target="_blank">миграционной сборкой</a> Vue.js 3
 
       <b-alert show variant="warning" class="mt-3">
-        @vue/compat support is designed for early migration to Vue.js 3 and will be eventually replaced with bootstrap-vue 3.0
+        Поддержка @vue/compat предназначена для ранней миграции на Vue.js 3 и в конечном итоге будет заменена на bootstrap-vue 3.0
       </b-alert>
       <h2 id="limitations" class="bv-no-focus-ring mb-3">
-        <span class="bd-content-title">Important limitations<a href="#prerequisites" aria-labelledby="limitations" class="anchorjs-link"></a></span>
+        <span class="bd-content-title">Важные ограничения<a href="#prerequisites" aria-labelledby="limitations" class="anchorjs-link"></a></span>
       </h2>
-      <p><code class="text-nowrap" translate="no">@vue-compat</code> support is currently limited to <code class="text-nowrap" translate="no">{ MODE: 2 }</code> configuration both for compiler and Vue.js itself.
-        You can find more details in <a href="https://v3-migration.vuejs.org/migration-build.html#compat-configuration" target="_blank">compat configuration</a> section of migration guide.
+      <p><code class="text-nowrap" translate="no">@vue-compat</code> в настоящее время ограничена конфигурацией <code class="text-nowrap" translate="no">{ MODE: 2 }</code> как для компилятора, так и для самого Vue.js.
+        Вы можете найти более подробную информацию в <a href="https://v3-migration.vuejs.org/migration-build.html#compat-configuration" target="_blank">разделе конфигурации</a> совместимости руководства по миграции.
       </p>
-      <p>That means that you can manually configure each your component with <code class="text-nowrap" translate="no">{ compatConfig: { MODE: 3 }}</code> to be ready for switching to Vue.js 3, however global configuration should be kept in legacy mode in order for all BootstrapVue functions to work</p>
+      <p>Это означает, что вы можете вручную настроить каждый свой компонент с помощью <code class="text-nowrap" translate="no">{ compatConfig: { MODE: 3 }}</code>, чтобы он был готов к переходу на Vue.js 3, однако глобальная конфигурация должна быть сохранена в устаревшем режиме, чтобы все функции BootstrapVue работали</p>
 
       <pre class="hljs language-html text-monospace p-2 notranslate" translate="no">{{ cmpCode }}</pre>
 
 
       <h2 id="start" class="bv-no-focus-ring">
-        <span class="bd-content-title">Getting started<a href="#prerequisites" aria-labelledby="start" class="anchorjs-link"></a></span>
+        <span class="bd-content-title">Начало работы<a href="#prerequisites" aria-labelledby="start" class="anchorjs-link"></a></span>
       </h2>
 
       <ul>
-        <li>Configure your application according to <a target="_blank" href="https://v3-migration.vuejs.org/migration-build.html#upgrade-workflow">upgrade workflow</a></li>
-        <li>Upgrade bootstrap-vue to <b>v2.23.0</b> or higher</li>
-        <li>Make sure you're still on bootstrap <b>v4</b></li>
+        <li>Настройте свое приложение в соответствии с <a target="_blank" href="https://v3-migration.vuejs.org/migration-build.html#upgrade-workflow">рабочим процессом обновления</a></li>
+        <li>Обновите bootstrap-vue до версии <b>v2.23.0</b> или выше</li>
+        <li>Убедитесь, что вы все еще используете bootstrap <b>v4</b></li>
       </ul>
 
-      <p>You can use this <a href="https://stackblitz.com/edit/bootstrap-vue-with-compat">sandbox</a> for reference or reporting issues with @vue/compat</p>
+      <p>Вы можете использовать эту <a href="https://stackblitz.com/edit/bootstrap-vue-with-compat">sandbox</a> для справки или сообщения о проблемах с @vue/compat</p>
 
     </section>
   </b-container>
@@ -44,7 +44,7 @@ import hljs from '../utils/hljs'
 
 export default {
   head() {
-    const title = `Vue.js 3 support | BootstrapVue`
+    const title = `Поддержка Vue.js 3 | BootstrapVue`
     return {
       title
     }
@@ -57,10 +57,10 @@ export default {
         `export default {`,
         `  data() { /* ... */ },`,
         ``,
-        `  // This will disable all Vue.js 2 legacy features for your component`,
+        `  // Это отключит все устаревшие функции Vue.js 2 для вашего компонента`,
         `  compatConfig: { MODE: 3 }`,
         ``,
-        ` // ... rest of your component configuration ... `,
+        ` // ... остальная часть конфигурации вашего компонента ... `,
         `}`,
         // eslint-disable-next-line no-useless-escape
         `<\/script>`
